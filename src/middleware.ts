8f726736524471c5ad37928df0a385ab9ee38a62
HonluePetnou <honluepetnou@gmail.com>
2026-05-05 01:21:36 +0100

    Update middleware matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,6 @@ export default createMiddleware({
 });
 
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(fr|en)/:path*']
+  // Match all pathnames except for api routes, _next, and files with dots
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
 };
